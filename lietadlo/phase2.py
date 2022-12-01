059cfdbf9b765fc1202c55905dc7747d852506ee
Peter Gubka <pgubka@gmail.com>
2022-12-01 14:41:24 +0100

    Add phase2.py

---
+++ lietadlo/phase2.py
@@ -0,0 +1,133 @@
+# Import the pygame module
+import pygame
+
+# Import random for random numbers
+import random
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+# from pygame.locals import *
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_a,
+    KEYDOWN,
+    QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+# Define the Player object extending pygame.sprite.Sprite
+# Instead of a surface, we use an image for a better looking sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("jet.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
+    # Move the sprite based on keypresses
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+
+# Define the enemy object extending pygame.sprite.Sprite
+# The surface we draw on the screen is now a property of 'enemy'
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("missile.png").convert()
+        #self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        # The starting position is randomly generated, as is the speed
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    # Move the enemy based on speed
+    # Remove it when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
+# Initialize pygame
+pygame.init()
+
+# Setup the clock for a decent framerate
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+# Create our 'player'
+player = Player()
+#enemies = pygame.sprite.Group()
+
+#all_sprites = pygame.sprite.Group()
+#all_sprites.add(player)
+
+#ADDENEMY = pygame.USEREVENT + 1
+#pygame.time.set_timer(ADDENEMY, 250)
+
+
+running = True
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop
+            if event.key == K_ESCAPE:
+                running = False
+            #if event.key == K_a:
+            #    new_enemy = Enemy()
+            #    enemies.add(new_enemy)
+        # Did the user click the window close button? If so, stop the loop
+        elif event.type == QUIT:
+            running = False
+        #elif event.type == ADDENEMY:
+        #    # Create the new enemy, and add it to our sprite groups
+        #    new_enemy = Enemy()
+        #    enemies.add(new_enemy)
+        #    all_sprites.add(new_enemy)
+
+
+    # Get the set of keys pressed and check for user input
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    enemies.update()
+
+    # Fill the screen with sky blue
+    screen.fill((135, 206, 250))
+
+    # Draw all our sprites
+    #for entity in all_sprites:
+    #screen.blit(entity.surf, entity.rect)
+    screen.blit(player.surf, player.rect)
+    #for enemy in enemies:
+    #    screen.blit(enemy.surf, enemy.rect)
+
+    # Flip everything to the display
+    pygame.display.flip()
+    clock.tick(30)
+
+# Done! Time to quit.
+pygame.quit()
