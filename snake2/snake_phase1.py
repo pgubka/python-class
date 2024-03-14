332471135c7e3d3375880a2702f6f66c4be7bfd4
Peter Gubka <pgubka@gmail.com>
2024-03-14 10:46:45 +0100

    Create snake_phase1.py

---
+++ snake2/snake_phase1.py
@@ -0,0 +1,14 @@
+import pygame
+#import time
+
+pygame.init()
+dis = pygame.display.set_mode((800, 600))
+#pygame.display.set_caption('Snake Game')
+pygame.display.update()
+
+#time.sleep(3)
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            game_over = True
+
+pygame.quit()
