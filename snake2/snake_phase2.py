20c1d848a8a6169b48256d980c030c79c8c04b63
Peter Gubka <pgubka@gmail.com>
2024-03-21 09:56:26 +0100

    Create snake_phase2.py

---
+++ snake2/snake_phase2.py
@@ -0,0 +1,43 @@
+import pygame
+ 
+pygame.init()
+ 
+#colours
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0,0,255)
+
+# display
+width=800
+hight=600
+dis = pygame.display.set_mode((width, hight))
+pygame.display.set_caption('Snake Game')
+ 
+game_over = False
+ 
+# snake
+x1 = width/2
+y1 = hight/2
+x1_change = 0       
+y1_change = 0
+ 
+clock = pygame.time.Clock()
+ 
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+ 
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis, blue, [x1, y1, 10, 10])
+ 
+    pygame.display.update()
+ 
+    clock.tick(30)
+ 
+pygame.quit()
+quit()
